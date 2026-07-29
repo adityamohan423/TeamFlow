5053a71d37caa9bc243b82484a93ab3541068f6c
Aditya Mohan <adityamohan423@gmail.com>
2026-07-29 17:42:16 +0530

    changed env name

--- teamflow-backend/config/db.js
+++ teamflow-backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.DB_URL);
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected !!");
   } catch (err) {
     console.log("connection error : ", err.message);
